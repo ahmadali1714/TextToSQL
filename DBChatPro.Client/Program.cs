0094cde8f792411029bfc80c9b2e230d9ac66049
ahmadali1714 <105272533+ahmadali1714@users.noreply.github.com>
2025-02-11 16:03:42 +0500

    Update Program.cs

--- DBChatPro.Client/Program.cs
+++ DBChatPro.Client/Program.cs
@@ -6,4 +6,5 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 
+
 await builder.Build().RunAsync();
